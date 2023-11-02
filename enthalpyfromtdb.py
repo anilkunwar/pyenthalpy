0f73619cb042bdf4886643a6fa03e51cf507cb90
Anil Kunwar <anilkunwar318@yahoo.com>
2023-11-02 19:55:49 +0100

    Update enthalpyfromtdb.py

--- enthalpyfromtdb.py
+++ enthalpyfromtdb.py
@@ -11,6 +11,8 @@ st.title("Equilibrium Calculation and Enthalpy Visualization")
 # Upload a .tdb file
 uploaded_file = st.file_uploader("Upload a .tdb file", type=["tdb"])
 
+equilibrium_results = []  # List to store equilibrium results
+
 if uploaded_file is not None:
     # Save the uploaded BytesIO object to a temporary file
     with tempfile.NamedTemporaryFile(delete=False) as tdb_file:
@@ -20,51 +22,84 @@ if uploaded_file is not None:
     # Process the uploaded .tdb file
     dbf = Database(tdb_path)
 
-    # Input field for selecting elements via a dropdown
-    st.sidebar.header("Select All the  Elements of the Alloy and Exclude VA here. It will be included later on.")
-    default_elements = list(dbf.elements)  # Use the elements from the database
-    selected_elements = st.multiselect("Select Elements", sorted(default_elements), default=default_elements)
+    condition_counter = 0  # Unique identifier for condition sets
 
-    # Remove the element that is alphabetically the farthest by default
-    # For N element alloy, the composition information of N-1 element will be sufficient
-    # This part will remove the information of the farthest element (in alphabetical order)
-    farthest_element = max(selected_elements)
-    selected_elements.remove(farthest_element)
+    while True:
+        condition_counter += 1  # Increment the condition set counter
 
-    # Input fields for equilibrium conditions
-    st.sidebar.header("Equilibrium Conditions")
-    user_temperature_start = st.sidebar.number_input("Starting Temperature (K)", 300, 2000, 300)
-    user_temperature_end = st.sidebar.number_input("Ending Temperature (K)", user_temperature_start, 1850, 1850)
-    user_temperature_step = st.sidebar.number_input("Temperature Step (K)", 1, 100, 10)
-    user_pressure = st.sidebar.number_input("Pressure (Pa)", 101325)
+        # Input field for selecting elements via a dropdown
+        st.sidebar.header("Select All the Elements of the Alloy and Exclude VA here. It will be included later on.")
+        default_elements = list(dbf.elements)  # Use the elements from the database
+        selected_elements = st.multiselect(
+            f"Select Elements (Set {condition_counter})",  # Add a unique identifier
+            sorted(default_elements),
+            default=default_elements
+        )
 
-    # Input fields for mole fractions (excluding 'VA')
-    user_mole_fractions = {}
-    for element in selected_elements:
-        if element != 'VA':
-            user_mole_fractions[element] = st.sidebar.number_input(f"Mole Fraction of {element}", 0.0, 1.0, 0.1)
+        # Remove the element that is alphabetically the farthest by default
+        # For N element alloy, the composition information of N-1 element will be sufficient
+        # This part will remove the information of the farthest element (in alphabetical order)
+        farthest_element = max(selected_elements)
+        selected_elements.remove(farthest_element)
 
-    # The composition information in eq_result i.e. selected_elements will have to include all of the elements + VA
-    # Include the element excluded from mole fractions in selected_elements
-    selected_elements.append(farthest_element)
-    # Append 'VA' to the selected_elements for eq_result
-    selected_elements.append('VA')
+        # Input fields for equilibrium conditions
+        st.sidebar.header("Equilibrium Conditions")
+        user_temperature_start = st.sidebar.number_input(
+            f"Starting Temperature (K) (Set {condition_counter})", 300, 2000, 300
+        )
+        user_temperature_end = st.sidebar.number_input(
+            f"Ending Temperature (K) (Set {condition_counter})", 1000, 3000, 1850
+        )
+        user_temperature_step = st.sidebar.number_input(
+            f"Temperature Step (K) (Set {condition_counter})", 1, 200, 10
+        )
+        user_pressure = st.sidebar.number_input(
+            f"Pressure (Pa) (Set {condition_counter})", 101325
+        )
 
-    # Perform the equilibrium calculation with user-defined conditions
-    user_conditions = {v.T: (user_temperature_start, user_temperature_end, user_temperature_step), v.P: user_pressure}
-    for element, fraction in user_mole_fractions.items():
-        if element != farthest_element:
-            user_conditions[v.X(element)] = fraction
+        # Input fields for mole fractions (excluding 'VA')
+        user_mole_fractions = {}
+        for element in selected_elements:
+            if element != 'VA':
+                user_mole_fractions[element] = st.sidebar.number_input(
+                    f"Mole Fraction of {element} (Set {condition_counter})", 0.0, 1.0, 0.1
+                )
 
-    # Equilibrium calculation with selected_elements (including 'VA') and user_conditions
-    eq_result = equilibrium(dbf, selected_elements, ['LIQUID', 'HCP_A3'], user_conditions, output='HM')
+        # The composition information in eq_result i.e. selected_elements will have to include all of the elements + VA
+        # Include the element excluded from mole fractions in selected_elements
+        selected_elements.append(farthest_element)
+        # Append 'VA' to the selected_elements for eq_result
+        selected_elements.append('VA')
 
-    # Extract values from the DataArray and then flatten
-    T_values = eq_result.T.values.flatten()
-    HM_values = eq_result.HM.values.flatten()
+        # Perform the equilibrium calculation with user-defined conditions
+        user_conditions = {v.T: (user_temperature_start, user_temperature_end, user_temperature_step), v.P: user_pressure}
+        for element, fraction in user_mole_fractions.items():
+            if element != farthest_element:
+                user_conditions[v.X(element)] = fraction
 
-    # Create a DataFrame from the flattened data
-    result_df = pd.DataFrame({'T': T_values, 'H': HM_values})
+        # Equilibrium calculation with selected_elements (including 'VA') and user_conditions
+        eq_result = equilibrium(dbf, selected_elements, ['LIQUID', 'HCP_A3'], user_conditions, output='HM')
+
+        # Extract values from the DataArray and then flatten
+        T_values = eq_result.T.values.flatten()
+        HM_values = eq_result.HM.values.flatten()
+
+        # Create a DataFrame from the flattened data
+        result_df = pd.DataFrame({'T': T_values, 'H': HM_values})
+
+        equilibrium_results.append(result_df)  # Append the result for this set of input values
+
+        # Create a unique key for the checkbox based on the condition set
+        add_more = st.sidebar.checkbox(
+            f"Add More Equilibrium Conditions? (Set {condition_counter + 1})",  # Add a unique identifier
+            key=f"add_more_{condition_counter}",
+        )
+
+        if not add_more:
+            break
+
+    # Concatenate all equilibrium results into a single DataFrame
+    combined_results_df = pd.concat(equilibrium_results, ignore_index=True)
 
     # Display a download link for the CSV file
     csv_file_path = 'equilibrium_results.csv'
@@ -72,7 +107,7 @@ if uploaded_file is not None:
     st.markdown("Click below to download the CSV results:")
     st.download_button(
         label="Download CSV",
-        data=result_df.to_csv(index=False).encode(),
+        data=combined_results_df.to_csv(index=False).encode(),
         key="download-csv",  # Unique key
         on_click=None,
         args=(csv_file_path,),
@@ -81,7 +116,9 @@ if uploaded_file is not None:
 
     # Create a line chart for H and T
     st.markdown("### Enthalpy vs. Temperature Chart")
-    st.line_chart(result_df.set_index('T')[['H']])
+    st.line_chart(combined_results_df.set_index('T')[['H']])
+
+
 
     # Plot the Enthalpy vs. Temperature
     st.pyplot(plt.figure(figsize=(8, 6)))
